fa073dc24c88eb9374a9dc938a69a2eae7ec304f
Sujal Karmakar <sujalkarmakar6363@gmail.com>
2024-03-08 18:13:28 +0530

    updated files

--- my-app/src/components/VideoCard.tsx
+++ my-app/src/components/VideoCard.tsx
@@ -1,21 +1,21 @@
 export function VideoCard(props: any) {
   return (
-    <div className="m-0 p-0">
-      <img src="/tumbnail.jpg" alt=""  className="mb-4 rounded-xl" />
+    <div className="m-0 p-3 cursor-pointer ">
+      <img src={props.thumbnail} alt=""  className="mb-4 rounded-xl" />
       <div className="grid grid-cols-12">
         <div className="col-span-1 pl-2 pt-1 ">
           <img
-            src={props.image}
+            src={props.logo}
             alt=""
-            className="rounded-full w-14 "
+            className="rounded-full w-15 "
           />
         </div>
-        <div className="col-span-11 ">
+        <div className="col-span-11 pl-2 ">
           <div>
             {props.title}
           </div>
           <div className="col-span-10 text-gray-400 text-base">{props.author}</div>
-          <div className="col-span-10 text-gray-400 text-base">{props.views} | {props.timestamp}</div>
+          <div className="col-span-10 text-gray-400 text-base">{props.viewCount} | {props.timestamp}</div>
         </div>
       </div>
     </div>
--- my-app/src/pages/index.tsx
+++ my-app/src/pages/index.tsx
@@ -1,13 +1,21 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import {VideoCard} from "../components/VideoCard"
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import { VideoCard } from '@/components/VideoCard'
+import { VIDEOS } from '@/videos'
+import { VideoGrid } from '@/components/VideoGrid'
+import { Appbar } from '@/components/AppBar'
+import { LeftBar } from '@/components/LeftBar'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div>
-      <VideoCard title={"'Portl' का Tech लगा Shark Aman को Jhakaas | Shark Tank India S2 | Pitches"} image={"logo.jpg"} author={"Sujal Karmakar"} views={"45M"} timestamp={"7 days ago"} ></VideoCard>
-    </div> 
+    <main>
+      <Appbar/>
+      <div className="flex">
+        <LeftBar />
+        <VideoGrid />
+      </div>
+    </main>
   )
 }
